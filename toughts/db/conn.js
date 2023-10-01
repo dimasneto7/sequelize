17ba5348c917e3e0a60e374ed4414d006be33c40
dimasneto7 <dimas_fcn@outlook.com>
2023-10-01 13:53:54 -0300

    toughts

--- toughts/db/conn.js
+++ toughts/db/conn.js
@@ -4,3 +4,12 @@ const sequelize = new Sequelize('toughts', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
 })
+
+try {
+  sequelize.authenticate()
+  console.log('Conectamos ao banco com sucesso!!!')
+} catch (err) {
+  console.log(`Não foi possível conectar ao banco: ${err}`)
+}
+
+module.exports = sequelize
--- toughts/index.js
+++ toughts/index.js
@@ -5,3 +5,61 @@ const FileStore = require('session-file-store')(session)
 const flash = require('express-flash')
 
 const app = express()
+
+const conn = require('./db/conn')
+
+// template engine
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+// receber resposta do body
+app.use(
+  express.urlenconded({
+    extended: true,
+  })
+)
+
+app.use(express.json())
+
+// session middleware
+app.use(
+  session({
+    name: 'session',
+    secret: 'nosso_secret',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require('path').join(require('os').tmpdir(), 'sessions'),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 360000),
+      httpOnly: true,
+    },
+  })
+)
+
+// Flash messages
+app.use(flash())
+
+// Public path
+app.use(express.static('public'))
+
+// set session to res
+app.use((req, res, next) => {
+
+  if.(req.session.userid) {
+    res.locals.session = req.session
+  }
+
+  next()
+})
+
+conn
+  .sync()
+  .then(() => {
+    app.listen(3000)
+  })
+  .catch((err) => console.log(err))
