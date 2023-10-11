b77f30bf3415961bd8325226f52162dcadd6018d
dimasneto7 <dimas_fcn@outlook.com>
2023-10-10 22:57:36 -0300

    delete toughts

--- toughts/controllers/ToughtController.js
+++ toughts/controllers/ToughtController.js
@@ -7,6 +7,63 @@ module.exports = class ToughtController {
   }
 
   static async dashboard(req, res) {
-    res.render('toughts/dashboard')
+    const userId = req.session.userid
+
+    const user = await User.findOne({
+      where: {
+        id: userId,
+      },
+      include: Tought,
+      plain: true,
+    })
+
+    // check if user exists
+    if (!user) {
+      res.redirect('/login')
+    }
+
+    const toughts = user.Toughts.map((result) => result.dataValues)
+
+    res.render('toughts/dashboard', { toughts })
+  }
+
+  static createTought(req, res) {
+    res.render('toughts/create')
+  }
+
+  static async createToughtSave(req, res) {
+    const tought = {
+      title: req.body.title,
+      UserId: req.session.userid,
+    }
+
+    try {
+      await Tought.create(tought)
+
+      req.flash('message', 'Pensamento criado com sucesso!')
+
+      req.session.save(() => {
+        res.redirect('/toughts/dashboard')
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  static async removeTought(req, res) {
+    const id = req.body.id
+    const UserId = req.session.userid
+
+    try {
+      await Tought.destroy({ where: { id: id, UserId: UserId } })
+
+      req.flash('message', 'Pensamento removido com sucesso!')
+
+      req.session.save(() => {
+        res.redirect('/toughts/dashboard')
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
--- toughts/routes/toughtsRoutes.js
+++ toughts/routes/toughtsRoutes.js
@@ -5,7 +5,10 @@ const ToughtController = require('../controllers/ToughtController')
 //  helpers
 const checkAuth = require('../helpers/auth').checkAuth
 
+router.get('/add', checkAuth, ToughtController.createTought)
+router.post('/add', checkAuth, ToughtController.createToughtSave)
 router.get('/dashboard', checkAuth, ToughtController.dashboard)
+router.post('/remove', checkAuth, ToughtController.removeTought)
 router.get('/', ToughtController.showToughts)
 
 module.exports = router
